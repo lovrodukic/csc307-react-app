68fa8a8e087de4fd810398c05ad73381fef1ebe1
Lovro Dukic <ldukic@calpoly.edu>
2023-01-17 17:17:05 -0800

    Deleted files

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -2,16 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
 const users = { 
   users_list :
   [
@@ -43,6 +33,39 @@ const users = {
   ]
 }
 
-app.get('/users', (req, res) => {
-  res.send(users);
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get('/users', (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+      let result = findUserByName(name);
+      result = {users_list: result};
+      res.send(result);
+  }
+  else {
+      res.send(users);
+  }
+});
+
+const findUserByName = (name) => { 
+  return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params['id']; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+  else {
+      result = {users_list: result};
+      res.send(result);
+  }
+});
+
+function findUserById(id) {
+  return users['users_list'].find( (user) => user['id'] === id);
+}
